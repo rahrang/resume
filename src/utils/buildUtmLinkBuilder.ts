d18fd1fc63dc8add9b23937e88c49e04840fb83a
Rahul Rangnekar <rahrang.dev@gmail.com>
2024-11-13 20:30:13 -0800

    set company via URL query params, ready to deploy

--- src/utils/buildUtmLinkBuilder.ts
+++ src/utils/buildUtmLinkBuilder.ts
@@ -1,10 +1,14 @@
+export const getCompanyId = (url: URL) => {
+  return url.searchParams.get("c") || url.searchParams.get("company");
+};
+
 export const buildUtmLinkBuilder = (companyId: string | null) => {
   return (href: string): string => {
     const url = new URL(href);
 
     url.searchParams.set("utm_campaign", companyId ?? "general");
     url.searchParams.set("utm_source", "resume");
-    url.searchParams.set("utm_medium", "pdf");
+    url.searchParams.set("utm_medium", companyId ? "pdf" : "web");
 
     return url.toString();
   };
